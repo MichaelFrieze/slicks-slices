1989ff760c08f6975cb852515b7b67e607a96b9c
Michael Frieze <michaelfrieze@icloud.com>
2020-11-24 17:53:50 -0500

    removed comments

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -2,26 +2,10 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-// these style only apply in this component (they are scoped)
-const NavStyles = styled.nav`
-  background: green;
-`;
-
-/* 
-Let's say we want to style tune unordered list (UL)
-There are 2 ways of doing it. 
-1. put ul {styles here} in the NavStyles above.
-2. Or, Make it's own styled component as demonstrated
-*/
-const UlStyles = styled.ul`
-  border: 10px solid yellow;
-`;
-// This way of doing it is good if you need to reuse it
-
 export default function Nav() {
   return (
     <NavStyles>
-      <UlStyles>
+      <ul>
         <li>
           <Link to="/">Hot Now</Link>
         </li>
@@ -37,7 +21,7 @@ export default function Nav() {
         <li>
           <Link to="/order">Order Ahead!</Link>
         </li>
-      </UlStyles>
+      </ul>
     </NavStyles>
   );
 }
