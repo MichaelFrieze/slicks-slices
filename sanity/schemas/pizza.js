5943622a2c2a7538df2c5f3bafbc723b3c5b38df
Michael Frieze <michaelfrieze@icloud.com>
2020-11-25 20:19:16 -0500

    creating data relationships

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -39,52 +39,6 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component
     },
-  ],
-};
-
-/* 
-import { MdLocalPizza as icon } from 'react-icons/md';
-import PriceInput from '../components/PriceInput';
-
-export default {
-  // Computer Name
-  name: 'pizza',
-  // visible title
-  title: 'Pizzas',
-  type: 'document',
-  icon,
-  fields: [
-    {
-      name: 'name',
-      title: 'Pizza Name',
-      type: 'string',
-      description: 'Name of the pizza',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 100,
-      },
-    },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'price',
-      title: 'Price',
-      type: 'number',
-      description: 'Price of the pizza in cents',
-      validation: Rule => Rule.min(1000),
-      inputComponent: PriceInput,
-    },
     {
       name: 'toppings',
       title: 'Toppings',
@@ -113,6 +67,3 @@ export default {
     },
   },
 };
-
-
-*/
