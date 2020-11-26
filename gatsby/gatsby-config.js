6a695354511db23385964b078a2285d38d6ce891
Michael Frieze <michaelfrieze@icloud.com>
2020-11-25 22:10:59 -0500

    sourcing sanity data

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
@@ -5,4 +9,17 @@ export default {
     description: 'The best pizza place in Hamilton!',
     twitter: '@michaelfrieze',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'mogfl0cc',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
