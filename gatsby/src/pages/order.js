a4d60a1b270ce4f69b1a86febcc7a3fc30d5f7df
Michael Frieze <michaelfrieze@icloud.com>
2020-11-29 18:26:38 -0500

    created a useForm utility | started building the order page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/.useForm';
 
 export default function OrderPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
   return (
     <>
       <SEO title="Order a Pizza!" />
-
-      <p>I am the order page</p>
+      <form>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
