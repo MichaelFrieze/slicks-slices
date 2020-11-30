ef9a53b7371964007b1f8f28673314ce0902fa4c
Michael Frieze <michaelfrieze@icloud.com>
2020-11-29 22:17:30 -0500

    created LoadingGrid component and updated Nav component

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -4,26 +4,21 @@ import styled from 'styled-components';
 import Logo from './Logo';
 
 const NavStyles = styled.nav`
-  margin-bottom: 3rem;
-
+  /* margin-bottom: 3rem; */
   .logo {
     transform: translateY(-25%);
   }
-
   ul {
     margin: 0;
     padding: 0;
     text-align: center;
     list-style: none;
-
     display: grid;
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
-
     margin-top: -6rem;
   }
-
   li {
     --rotate: -2deg;
     transform: rotate(var(--rotate));
@@ -41,17 +36,40 @@ const NavStyles = styled.nav`
       --rotate: 3deg;
     }
   }
-
   a {
     font-size: 3rem;
     text-decoration: none;
+    display: block;
     &:hover {
       color: var(--red);
     }
-
+    @media (max-width: 800px) {
+      font-size: 2rem;
+    }
     /* &[aria-current='page'] {
       color: var(--red);
     } */
+  }
+  @media (max-width: 600px) {
+    --columns: 4;
+    margin-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+    padding-bottom: 2rem;
+    ul {
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+    .logo {
+      transform: none;
+    }
+  }
+  @media (max-width: 500px) {
+    --columns: 2;
   }
 `;
 
@@ -63,9 +81,9 @@ export default function Nav() {
           <Link to="/">Hot Now</Link>
         </li>
         <li>
-          <Link to="/pizzas">Pizza Menu</Link>
+          <Link to="/pizzas/">Pizza Menu</Link>
         </li>
-        <li>
+        <li className="logo-item">
           <Link to="/">
             <Logo />
           </Link>
